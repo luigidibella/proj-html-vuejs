2b94e33f0c8e71fe4d2515dc102aa69cf3eaa881
luigidibella <lykos.omega@gmail.com>
2024-04-09 12:57:51 +0200

    [add] Amazon content

--- src/components/partials/Amazon.vue
+++ src/components/partials/Amazon.vue
@@ -5,11 +5,28 @@ export default {
 </script>
 
 <template>
-  <div class="container text-center">
-    <h3>Amazon</h3>
+  <div class="container-fluid px-0 bg_img d-flex justify-content-center align-item-center">
+    <!-- <img src="../../assets/images/slider_1-1917x607-1914x606.jpg" alt=""> -->
+    <div class="row row-cols-2 my_container pt-5">
+      <div class="col d-flex flex-column justify-content-center align-item-center">
+        <h2 class="text-warning mb-3 fs-1">Get My Exclusive Investing Book just fot $19.50</h2>
+        <p class="text-white fs-4">Vestibulum imentum eu urna ve/ cursus. Aenean vehicula quam quam, vitae pel/entesque urna elementum cursus.</p>
+        <a href="">
+          <img src="../../assets/images/amazon.png" alt="">
+        </a>
+      </div>
+      <div class="col">
+        <img src="../../assets/images/book.png" alt="">
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+
+.bg_img{
+  background-image: url(../../assets/images/slider_1-1917x607-1914x606.jpg);
+  background-size: cover;
+}
 
 </style>
