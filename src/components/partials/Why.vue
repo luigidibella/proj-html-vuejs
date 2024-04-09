820cefffe7ca1a5025f23481e9d032ee7998ac43
luigidibella <lykos.omega@gmail.com>
2024-04-09 13:32:37 +0200

    [add] Why content

--- src/components/partials/Why.vue
+++ src/components/partials/Why.vue
@@ -5,11 +5,97 @@ export default {
 </script>
 
 <template>
-  <div class="container text-center">
-    <h3>Why</h3>
+  <div class="container-fluid d-flex flex-column justify-content-center align-items-center py-4">
+    <div class="section_title my-4">
+      <h2>Why my Courses?</h2>
+    </div>
+
+    <div class="row row-cols-2 my_container">
+      <div class="col">
+        <div class="card my_card mb-3">
+          <div class="row g-0">
+            <div class="col-md-2">
+              <div class="icon py-3">
+                <i class="fa-solid fa-suitcase"></i>
+              </div>
+            </div>
+            <div class="col-md-9">
+              <div class="card-body text-start px-0">
+                <h5 class="card-title">Enterpreneurship</h5>
+                <p class="card-text">estibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra urus. Pellentesque nec posuere ligula, eu congue leo. Inte ervul utate tem or arcu. Vestibulum vul utate</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card my_card mb-3">
+          <div class="row g-0">
+            <div class="col-md-2">
+              <div class="icon py-3">
+                <i class="fa-solid fa-suitcase"></i>
+              </div>
+            </div>
+            <div class="col-md-9">
+              <div class="card-body text-start px-0">
+                <h5 class="card-title">Enterpreneurship</h5>
+                <p class="card-text">estibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra urus. Pellentesque nec posuere ligula, eu congue leo. Inte ervul utate tem or arcu. Vestibulum vul utate</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card my_card mb-3">
+          <div class="row g-0">
+            <div class="col-md-2">
+              <div class="icon py-3">
+                <i class="fa-solid fa-suitcase"></i>
+              </div>
+            </div>
+            <div class="col-md-9">
+              <div class="card-body text-start px-0">
+                <h5 class="card-title">Enterpreneurship</h5>
+                <p class="card-text">estibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra urus. Pellentesque nec posuere ligula, eu congue leo. Inte ervul utate tem or arcu. Vestibulum vul utate</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card my_card mb-3">
+          <div class="row g-0">
+            <div class="col-md-2">
+              <div class="icon py-3">
+                <i class="fa-solid fa-suitcase"></i>
+              </div>
+            </div>
+            <div class="col-md-9">
+              <div class="card-body text-start px-0">
+                <h5 class="card-title">Enterpreneurship</h5>
+                <p class="card-text">estibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra urus. Pellentesque nec posuere ligula, eu congue leo. Inte ervul utate tem or arcu. Vestibulum vul utate</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 
+.col{
+  /* border: 1px solid greenyellow; */
+}
+
+.my_card{
+  border-left: 5px solid #f2b91e;
+  border-radius: 0;
+}
+
+.icon{
+  color: #f2b91e;
+  font-size: 2.5rem;
+}
 </style>
