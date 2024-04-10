e3146e14765b63090b228065bfbfbcfb2dc79865
luigidibella <lykos.omega@gmail.com>
2024-04-10 09:47:08 +0200

    [add] BenefitCard to sub_partials

--- src/components/partials/Why.vue
+++ src/components/partials/Why.vue
@@ -1,5 +1,10 @@
 <script>
+import BenefitCard from './sub_partials/BenefitCard.vue'
+
 export default {
+  components:{
+    BenefitCard
+  }
   
 }
 </script>
@@ -11,74 +16,12 @@ export default {
     </div>
 
     <div class="row row-cols-2 my_container">
-      <div class="col">
-        <div class="card my_card mb-3 py-3">
-          <div class="row g-0">
-            <div class="col-md-2">
-              <div class="icon py-3">
-                <i class="fa-solid fa-suitcase"></i>
-              </div>
-            </div>
-            <div class="col-md-9">
-              <div class="card-body text-start px-0">
-                <h5 class="card-title">Enterpreneurship</h5>
-                <p class="card-text">estibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra urus. Pellentesque nec posuere ligula, eu congue leo. Inte ervul utate tem or arcu. Vestibulum vul utate</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card my_card mb-3 py-3">
-          <div class="row g-0">
-            <div class="col-md-2">
-              <div class="icon py-3">
-                <i class="fa-solid fa-suitcase"></i>
-              </div>
-            </div>
-            <div class="col-md-9">
-              <div class="card-body text-start px-0">
-                <h5 class="card-title">Enterpreneurship</h5>
-                <p class="card-text">estibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra urus. Pellentesque nec posuere ligula, eu congue leo. Inte ervul utate tem or arcu. Vestibulum vul utate</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card my_card mb-3 py-3">
-          <div class="row g-0">
-            <div class="col-md-2">
-              <div class="icon py-3">
-                <i class="fa-solid fa-suitcase"></i>
-              </div>
-            </div>
-            <div class="col-md-9">
-              <div class="card-body text-start px-0">
-                <h5 class="card-title">Enterpreneurship</h5>
-                <p class="card-text">estibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra urus. Pellentesque nec posuere ligula, eu congue leo. Inte ervul utate tem or arcu. Vestibulum vul utate</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card my_card mb-3 py-3">
-          <div class="row g-0">
-            <div class="col-md-2">
-              <div class="icon py-3">
-                <i class="fa-solid fa-suitcase"></i>
-              </div>
-            </div>
-            <div class="col-md-9">
-              <div class="card-body text-start px-0">
-                <h5 class="card-title">Enterpreneurship</h5>
-                <p class="card-text">estibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra urus. Pellentesque nec posuere ligula, eu congue leo. Inte ervul utate tem or arcu. Vestibulum vul utate</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      
+      <BenefitCard />
+      <BenefitCard />
+      <BenefitCard />
+      <BenefitCard />
+
     </div>
   </div>
 </template>
@@ -86,26 +29,5 @@ export default {
 <style lang="scss" scoped>
 
 @use '../../assets/scss/partials/variables' as *;
-
-.my_card{
-  border-left: 5px solid $Buttercup;
-  border-top: 5px solid transparent;
-  border-right: 5px solid transparent;
-  border-bottom: 5px solid transparent;
-  border-radius: 0;
-  box-shadow: 0px 0px 10px rgb(0, 0, 0, 0.3);
-  &:hover{
-    border-color: $White;
-    background-color: $Buttercup;
-    .icon{
-      color: $White;
-    }
-  }
-}
-
-.icon{
-  color: $Buttercup;
-  font-size: 2.5rem;
-}
 
 </style>
---
+++ src/components/partials/sub_partials/BenefitCard.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+  
+}
+</script>
+
+<template>
+  <div class="col">
+    <div class="card my_card mb-3 py-3">
+      <div class="row g-0">
+        <div class="col-md-2">
+          <div class="icon py-3">
+            <i class="fa-solid fa-suitcase"></i>
+          </div>
+        </div>
+        <div class="col-md-9">
+          <div class="card-body text-start px-0">
+            <h5 class="card-title">Enterpreneurship</h5>
+            <p class="card-text">estibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra urus. Pellentesque nec posuere ligula, eu congue leo. Inte ervul utate tem or arcu. Vestibulum vul utate</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+@use '../../../assets/scss/partials/variables' as *;
+
+.my_card{
+  border-left: 5px solid $Buttercup;
+  border-top: 5px solid transparent;
+  border-right: 5px solid transparent;
+  border-bottom: 5px solid transparent;
+  border-radius: 0;
+  box-shadow: 0px 0px 10px rgb(0, 0, 0, 0.3);
+  .icon{
+    color: $Buttercup;
+    font-size: 2.5rem;
+  }
+  &:hover{
+    border-color: $White;
+    background-color: $Buttercup;
+    .icon{
+      color: $White;
+    }
+  }
+}
+
+</style>
