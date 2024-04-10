5f8fbf1bf347d7014d5ddb5679aa4d49cdf25586
luigidibella <lykos.omega@gmail.com>
2024-04-10 09:33:08 +0200

    [add] CourseCard to sub_partials

--- src/components/partials/NewCourses.vue
+++ src/components/partials/NewCourses.vue
@@ -1,7 +1,13 @@
 <script>
+import CourseCard from './sub_partials/CourseCard.vue'
+
 export default {
+  components:{
+    CourseCard
+  }
   
 }
+
 </script>
 
 <template>
@@ -11,252 +17,23 @@ export default {
     </div> 
 
     <div class="row row-cols-3 my_container">
-      
-      <div class="col py-2">
-        <div class="card position-relative">
-          <!-- Price -->
-          <div class="price position-absolute d-flex justify-content-center">
-            <strong class="text-white fs-3">$59</strong>
-          </div>
-          <!-- /Price -->
-          
-          <img class="card-img-top" src="../../assets/images/photo-1496307042754-b4aa456c4a2d-370x200.jpeg" alt="Card image cap">
-          
-          <span class="position-absolute top-0 end-0 badge bg-warning m-2">
-            SPECIAL
-          </span>
-          
-          <div class="card-body">
-            <h5 class="card-title">How to be a DJ? Make Electronic Music</h5>
-            <p class="card-text type">Electronic</p>
-            <hr>
-            <div class="options flex-center">
-              <div class="level me-2">
-                <i class="fa-solid fa-signal color-buttercup"></i>
-                <span> Advanced</span>
-              </div>
-              <div class="lectures me-2">
-                <i class="fa-solid fa-list-ul color-buttercup"></i>
-                <span> 8 Lectures</span>
-              </div>
-              <div class="hours">
-                <i class="fa-regular fa-clock color-buttercup"></i>
-                <span> 6 Hours</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col py-2">
-        <div class="card position-relative">
-          <!-- Price -->
-          <div class="price position-absolute d-flex justify-content-center">
-            <strong class="text-white fs-3">$59</strong>
-          </div>
-          <!-- /Price -->
-          
-          <img class="card-img-top" src="../../assets/images/photo-1496307042754-b4aa456c4a2d-370x200.jpeg" alt="Card image cap">
-          
-          <span class="position-absolute top-0 end-0 badge bg-warning m-2">
-            SPECIAL
-          </span>
-          
-          <div class="card-body">
-            <h5 class="card-title">How to be a DJ? Make Electronic Music</h5>
-            <p class="card-text type">Electronic</p>
-            <hr>
-            <div class="options flex-center">
-              <div class="level me-2">
-                <i class="fa-solid fa-signal color-buttercup"></i>
-                <span> Advanced</span>
-              </div>
-              <div class="lectures me-2">
-                <i class="fa-solid fa-list-ul color-buttercup"></i>
-                <span> 8 Lectures</span>
-              </div>
-              <div class="hours">
-                <i class="fa-regular fa-clock color-buttercup"></i>
-                <span> 6 Hours</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col py-2">
-        <div class="card position-relative">
-          <!-- Price -->
-          <div class="price position-absolute d-flex justify-content-center">
-            <strong class="text-white fs-3">$59</strong>
-          </div>
-          <!-- /Price -->
-          
-          <img class="card-img-top" src="../../assets/images/photo-1496307042754-b4aa456c4a2d-370x200.jpeg" alt="Card image cap">
-          
-          <span class="position-absolute top-0 end-0 badge bg-warning m-2">
-            SPECIAL
-          </span>
-          
-          <div class="card-body">
-            <h5 class="card-title">How to be a DJ? Make Electronic Music</h5>
-            <p class="card-text type">Electronic</p>
-            <hr>
-            <div class="options flex-center">
-              <div class="level me-2">
-                <i class="fa-solid fa-signal color-buttercup"></i>
-                <span> Advanced</span>
-              </div>
-              <div class="lectures me-2">
-                <i class="fa-solid fa-list-ul color-buttercup"></i>
-                <span> 8 Lectures</span>
-              </div>
-              <div class="hours">
-                <i class="fa-regular fa-clock color-buttercup"></i>
-                <span> 6 Hours</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col py-2">
-        <div class="card position-relative">
-          <!-- Price -->
-          <div class="price position-absolute d-flex justify-content-center">
-            <strong class="text-white fs-3">$59</strong>
-          </div>
-          <!-- /Price -->
-          
-          <img class="card-img-top" src="../../assets/images/photo-1496307042754-b4aa456c4a2d-370x200.jpeg" alt="Card image cap">
-          
-          <span class="position-absolute top-0 end-0 badge bg-warning m-2">
-            SPECIAL
-          </span>
-          
-          <div class="card-body">
-            <h5 class="card-title">How to be a DJ? Make Electronic Music</h5>
-            <p class="card-text type">Electronic</p>
-            <hr>
-            <div class="options flex-center">
-              <div class="level me-2">
-                <i class="fa-solid fa-signal color-buttercup"></i>
-                <span> Advanced</span>
-              </div>
-              <div class="lectures me-2">
-                <i class="fa-solid fa-list-ul color-buttercup"></i>
-                <span> 8 Lectures</span>
-              </div>
-              <div class="hours">
-                <i class="fa-regular fa-clock color-buttercup"></i>
-                <span> 6 Hours</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col py-2">
-        <div class="card position-relative">
-          <!-- Price -->
-          <div class="price position-absolute d-flex justify-content-center">
-            <strong class="text-white fs-3">$59</strong>
-          </div>
-          <!-- /Price -->
-          
-          <img class="card-img-top" src="../../assets/images/photo-1496307042754-b4aa456c4a2d-370x200.jpeg" alt="Card image cap">
-          
-          <span class="position-absolute top-0 end-0 badge bg-warning m-2">
-            SPECIAL
-          </span>
-          
-          <div class="card-body">
-            <h5 class="card-title">How to be a DJ? Make Electronic Music</h5>
-            <p class="card-text type">Electronic</p>
-            <hr>
-            <div class="options flex-center">
-              <div class="level me-2">
-                <i class="fa-solid fa-signal color-buttercup"></i>
-                <span> Advanced</span>
-              </div>
-              <div class="lectures me-2">
-                <i class="fa-solid fa-list-ul color-buttercup"></i>
-                <span> 8 Lectures</span>
-              </div>
-              <div class="hours">
-                <i class="fa-regular fa-clock color-buttercup"></i>
-                <span> 6 Hours</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col py-2">
-        <div class="card position-relative">
-          <!-- Price -->
-          <div class="price position-absolute d-flex justify-content-center">
-            <strong class="text-white fs-3">$59</strong>
-          </div>
-          <!-- /Price -->
-          
-          <img class="card-img-top" src="../../assets/images/photo-1496307042754-b4aa456c4a2d-370x200.jpeg" alt="Card image cap">
-          
-          <span class="position-absolute top-0 end-0 badge bg-warning m-2">
-            SPECIAL
-          </span>
-          
-          <div class="card-body">
-            <h5 class="card-title">How to be a DJ? Make Electronic Music</h5>
-            <p class="card-text type">Electronic</p>
-            <hr>
-            <div class="options flex-center">
-              <div class="level me-2">
-                <i class="fa-solid fa-signal color-buttercup"></i>
-                <span> Advanced</span>
-              </div>
-              <div class="lectures me-2">
-                <i class="fa-solid fa-list-ul color-buttercup"></i>
-                <span> 8 Lectures</span>
-              </div>
-              <div class="hours">
-                <i class="fa-regular fa-clock color-buttercup"></i>
-                <span> 6 Hours</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+
+      <CourseCard />
+      <CourseCard />
+      <CourseCard />
+      <CourseCard />
+      <CourseCard />
+      <CourseCard />
 
     </div>
 
-    <div class="btn btn-warning rounded-5 mt-3">LOAD MORE</div>
+    <div class="btn btn-warning rounded-5 m-3">LOAD MORE</div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 
 @use '../../assets/scss/partials/variables' as *;
-
-.price{
-  width: 100%;
-  height: 100%;
-  border-radius: 0.375rem;
-  strong{
-    display: none;
-    position: absolute;
-    top: 25%;
-  }
-  
-  &:hover{
-    background-color: rgb(0, 0, 0, 0.3);
-    strong{
-      display: flex;
-    }
-  }
-}
-.type{
-  color: $Silver;
-}
-
-.color-buttercup{
-  color: $Buttercup;
-}
 
 .btn{
   color: $White;
---
+++ src/components/partials/sub_partials/CourseCard.vue
@@ -0,0 +1,74 @@
+<script>
+export default {
+  
+}
+</script>
+
+<template>
+  <div class="col mb-4">
+    <div class="card position-relative">
+      <!-- Price -->
+      <div class="price position-absolute d-flex justify-content-center">
+        <strong class="text-white fs-3">$59</strong>
+      </div>
+      <!-- /Price -->
+      
+      <img class="card-img-top" src="../../../assets/images/photo-1496307042754-b4aa456c4a2d-370x200.jpeg" alt="Card image cap">
+      
+      <span class="position-absolute top-0 end-0 badge bg-warning m-2">
+        SPECIAL
+      </span>
+      
+      <div class="card-body">
+        <h5 class="card-title">How to be a DJ? Make Electronic Music</h5>
+        <p class="card-text type">Electronic</p>
+        <hr>
+        <div class="options flex-center">
+          <div class="level me-2">
+            <i class="fa-solid fa-signal color-buttercup"></i>
+            <span> Advanced</span>
+          </div>
+          <div class="lectures me-2">
+            <i class="fa-solid fa-list-ul color-buttercup"></i>
+            <span> 8 Lectures</span>
+          </div>
+          <div class="hours">
+            <i class="fa-regular fa-clock color-buttercup"></i>
+            <span> 6 Hours</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+@use '../../../assets/scss/partials/variables' as *;
+
+.price{
+  width: 100%;
+  height: 100%;
+  border-radius: 0.375rem;
+  strong{
+    display: none;
+    position: absolute;
+    top: 25%;
+  }
+  &:hover{
+    background-color: rgb(0, 0, 0, 0.3);
+    strong{
+      display: flex;
+    }
+  }
+}
+
+.type{
+  color: $Silver;
+}
+
+.color-buttercup{
+  color: $Buttercup;
+}
+
+</style>
