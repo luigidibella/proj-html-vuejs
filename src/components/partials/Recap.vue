71fdf6dc259f769326f7e7718ff58f0d7dad437b
luigidibella <lykos.omega@gmail.com>
2024-04-10 10:12:55 +0200

    [add] RecapCard to sub_partials

--- src/components/partials/Recap.vue
+++ src/components/partials/Recap.vue
@@ -1,5 +1,10 @@
 <script>
+import RecapCard from './sub_partials/RecapCard.vue'
+
 export default {
+  components:{
+    RecapCard
+  }
   
 }
 </script>
@@ -7,75 +12,12 @@ export default {
 <template>
   <div class="container-fluid px-0 bg_img flex-center">
     <div class="row row-cols-5 my_container">
-      <div class="col flex-center">
-        <div class="icon me-3 flex-center">
-          <i class="fa-solid fa-user-group fs-1"></i>
-        </div>
-        <div class="info d-flex flex-column text-start">
-          <div class="num">
-            <h2>3195</h2>
-          </div>
-          <div class="data">
-            <span>Online Learner</span>
-          </div>
-        </div>
-      </div>
-      
-      <div class="col flex-center">
-        <div class="icon me-3 flex-center">
-          <i class="fa-solid fa-user-group fs-1"></i>
-        </div>
-        <div class="info d-flex flex-column text-start">
-          <div class="num">
-            <h2>3195</h2>
-          </div>
-          <div class="data">
-            <span>Online Learner</span>
-          </div>
-        </div>
-      </div>
-      
-      <div class="col flex-center">
-        <div class="icon me-3 flex-center">
-          <i class="fa-solid fa-user-group fs-1"></i>
-        </div>
-        <div class="info d-flex flex-column text-start">
-          <div class="num">
-            <h2>3195</h2>
-          </div>
-          <div class="data">
-            <span>Online Learner</span>
-          </div>
-        </div>
-      </div>
-      
-      <div class="col flex-center">
-        <div class="icon me-3 flex-center">
-          <i class="fa-solid fa-user-group fs-1"></i>
-        </div>
-        <div class="info d-flex flex-column text-start">
-          <div class="num">
-            <h2>3195</h2>
-          </div>
-          <div class="data">
-            <span>Online Learner</span>
-          </div>
-        </div>
-      </div>
-      
-      <div class="col flex-center">
-        <div class="icon me-3 flex-center">
-          <i class="fa-solid fa-user-group fs-1"></i>
-        </div>
-        <div class="info d-flex flex-column text-start">
-          <div class="num">
-            <h2>3195</h2>
-          </div>
-          <div class="data">
-            <span>Online Learner</span>
-          </div>
-        </div>
-      </div>
+
+      <RecapCard />
+      <RecapCard />
+      <RecapCard />
+      <RecapCard />
+      <RecapCard />
       
     </div>
   </div>
@@ -84,15 +26,6 @@ export default {
 <style lang="scss" scoped>
 
 @use '../../assets/scss/partials/variables' as *;
-
-h2{
-  color: $Buttercup;
-}
-
-.icon,
-.data{
-  color: $White;
-}
 
 .bg_img{
   height: 600px;
---
+++ src/components/partials/sub_partials/RecapCard.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  
+}
+</script>
+
+<template>
+  <div class="col flex-center">
+    <div class="icon me-3 flex-center">
+      <i class="fa-solid fa-user-group fs-1"></i>
+    </div>
+    <div class="info d-flex flex-column text-start">
+      <div class="num">
+        <h2>3195</h2>
+      </div>
+      <div class="data">
+        <span>Online Learner</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+@use '../../../assets/scss/partials/variables' as *;
+
+h2{
+  color: $Buttercup;
+}
+
+.icon,
+.data{
+  color: $White;
+}
+
+</style>
